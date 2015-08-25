6cb8e888e18a4cf90bed36bd0bbfed86b0a121d7
Neill Bogie <neillbogie@googlemail.com>
2015-08-25 14:46:20 +0100

    working

--- assets/js/mymp3mixer.js
+++ assets/js/mymp3mixer.js
@@ -1,18 +1,19 @@
+"use strict";
 var gBufferList;
 var context;
 var bufferLoader;
-var source1, source2, source3;
-var gainNode1, gainNode2, gainNode3;
+var sourceAndGainPairs;
+var isPlaying; //Fixme: ask the API, instead
+
 var posOffset = 0;
 var playStartedTime = -1;
 var playStartedOffset; // a snapshot of posOffset at start of current play
-window.onload=initmp3mixer;
  
 function BufferLoader(context, urlList, callback) {
   this.context = context;
   this.urlList = urlList;
   this.onload = callback;
-  this.bufferList = new Array();
+  this.bufferList = [];
   this.loadCount = 0;
 }
 
@@ -41,19 +42,19 @@ BufferLoader.prototype.loadBuffer = function(url, index) {
         console.error('decodeAudioData error', error);
       }
     );
-  }
+  };
 
   request.onerror = function() {
     alert('BufferLoader: XHR error');
-  }
+  };
 
   request.send();
-}
+};
 
 BufferLoader.prototype.load = function() {
   for (var i = 0; i < this.urlList.length; ++i)
   this.loadBuffer(this.urlList[i], i);
-}
+};
 
 
 
@@ -63,18 +64,18 @@ function initmp3mixer() {
   // Fix up prefixing
   window.AudioContext = window.AudioContext || window.webkitAudioContext;
   context = new AudioContext();
-
+  isPlaying = false;
+  var songDir = "deep_river"; //"as";
   bufferLoader = new BufferLoader(
     context,
-    [
-      'sounds/as/soprano.mp3',
-      'sounds/as/alto.mp3',
-      'sounds/as/tenor.mp3',
-    ],
+    ['soprano', 'alto', 'tenor'].map(function(n) {
+      return "sounds/" + songDir + "/" + n + ".mp3";
+    }),
     finishedLoading
     );
 
   bufferLoader.load();
+
 }
 
 
@@ -87,29 +88,23 @@ function createBuffer(b){
 function createGainedBuffer(b){
   var src = createBuffer(b);
   var gainNode = linkThroughGain(src);
-  return [src, gainNode];
+  return {src: src, gainNode: gainNode};
 }
 
 function createAllBuffers(bufferList){
-  [source1, gainNode1] = createGainedBuffer(bufferList[0]);
-  [source2, gainNode2] = createGainedBuffer(bufferList[1]);
-  [source3, gainNode3] = createGainedBuffer(bufferList[2]);
+  sourceAndGainPairs = bufferList.map(function (buf) { 
+    return createGainedBuffer(buf);
+  });
 }
-
 
 function finishedLoading(bufferList) {
   gBufferList = bufferList;
   // Create three sources and play them both together.
-  //createAllBuffers(bufferList);
+  createAllBuffers(bufferList);
   //play();
 }
 function wipeAllBuffers(){
-  source1 = null;
-  source2 = null;
-  source3 = null;
-  gainNode1 = null;
-  gainNode2 = null;
-  gainNode3 = null;
+  sourceAndGainPairs = [];
 }
 
 function linkThroughGain(src){
@@ -127,24 +122,14 @@ function changeVolume(thing){
   var numstr = thing.id.substring(3,4);
   var num = parseInt(numstr);
   var val = parseInt(thing.value);
-  //console.log("changeVolume "+ thing.id + " val: " + thing.value + " value: " + val);
   
-  var node;
-  switch(num) {
-    case 1:
-        node = gainNode1;
-        break;
-    case 2:
-        node = gainNode2;
-        break;
-    case 3:
-        node = gainNode3;
-        break;
-    default:
-        console.log("ERROR: unexpected num: "+ num + " from id " + thing.id );
-    }    
-    node.gain.value = val/100.0;
-    //console.log("node gain is now: " + node.gain.value);
+
+  var pair = sourceAndGainPairs[num - 1];
+  console.log("changeVolume "+ thing.id + " num: "+ num +" val: " + thing.value + " value: " + val + " and val-1 is " + (val -1) + " and that pair is " + pair);
+  
+  var gainNode = pair.gainNode;
+  gainNode.gain.value = val/100.0;
+  //console.log("node gain is now: " + gainNode.gain.value);
 
 }
 function lengthOfSourceInSec(){
@@ -164,22 +149,30 @@ function posSliderToSeconds(elem){
 
 function stop(){
   playStartedTime = -1;
-  if(source1){
-    source1.stop(0);
-    source2.stop(0);
-    source3.stop(0);
+  var firstSource = sourceAndGainPairs[0].src;
+  if(firstSource !== null){
+    sourceAndGainPairs.forEach(function(pair) {
+      if (pair.src !== null) {
+        pair.src.stop(0);
+      }
+    } );
+    isPlaying = false;
     wipeAllBuffers();
   }
 }
  
 function play(){
-  stop();
+  if (isPlaying){
+    stop();
+    isPlaying = false;    
+  }
   createAllBuffers(gBufferList);
   playStartedTime = context.currentTime;
   playStartedOffset = posOffset;
-  source1.start(0, posOffset);
-  source2.start(0, posOffset);
-  source3.start(0, posOffset);
+  sourceAndGainPairs.forEach(function(pair) {
+    pair.src.start(0, posOffset);
+  } );
+  isPlaying = true;
 }
 
 function snapshotTime(){
